9c5e784fcc017cf23042fe2e730221d593d74e3d
DerekWu-lanxuanfeng <derek6616@vip.qq.com>
2016-05-08 17:13:44 +0800

    打开项目新增加了不少文件

--- Sample-wing/src/Main.ts
+++ Sample-wing/src/Main.ts
@@ -3,5 +3,19 @@ class Main extends egret.DisplayObjectContainer {
     public constructor() {
         super();
         var sample = new SamplePointLight ();
+         // var sample = new SampleAnimation();
+//        var sample = new SampleCreateCube();
+//        var sample = new SampleCubeTransform();
+//        var sample = new SampleCullMode();
+//        var sample = new SampleDirLight();
+//        var sample = new SampleEgret3DCanvas();
+//        var sample = new SampleEvent();   
+//        var sample = new SampleGeometryUtil();
+//        var sample = new SampleMultipleView3D();
+//        var sample = new SamplePointLight();
+//        var sample = new SampleSky();
+//        var sample = new SampleStaticModel();
+//        var sample = new SampleTextureCube();
+//        var sample = new SampleView3D(); 
     }
 } 
